b65ffe0acdf87c83d1bd870bc390b83495bad7a7
Shishir Jha <shishirjhaccr@gmail.com>
2020-09-27 06:16:56 +0530

    submitted code

--- pratilipichallenge/connect4/serializers.py
+++ pratilipichallenge/connect4/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
+from .models import Game
 
 
-class ResultSerializer(serializers.Serializer):
-    readiness = serializers.CharField(read_only=True)
-    validity = serializers.CharField(read_only=True)
+class GameSerializer(serializers.ModelSerializer):
+
+    column = serializers.IntegerField()
+
+    def get_column(self, obj):
+        return obj.get_column()
+
+    class Meta:
+        model = Game
+        fields = ('column', 'player')
+        read_only_fields = ('validity',)
--- pratilipichallenge/pratilipichallenge/settings.py
+++ pratilipichallenge/pratilipichallenge/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'pratilipichallenge.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pratilipi',
+        'NAME': 'pratilipi2',
         'USER': 'postgres',
         'PASSWORD': '9814721168',
         'HOST': '127.0.0.1',
